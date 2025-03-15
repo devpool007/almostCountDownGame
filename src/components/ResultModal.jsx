9a3250e29acfbb0227aa65a47c7726f923fb3571
Devansh Sharma <sharmadevansh007777@gmail.com>
2025-03-15 18:27:44 +0100

    game completed with Refs and states setup

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,14 +1,29 @@
-
+import { useImperativeHandle, useRef } from "react";
 
 //In older React versions you need to wrap the component with forwardRef to access the ref prop.
-export default function ResultModal({ ref, result, targetTime}) {
+export default function ResultModal({ ref, targetTime, remainingTime, onReset }) {
+
+  const userLost = remainingTime <= 0;
+  const formattedTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round(( 1 - remainingTime / (targetTime * 1000))*100);
+
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      }
+    };
+  }); 
 
   return (
-    <dialog ref={ref} className="result-modal">
-     <h2> You {result} </h2>
+    <dialog onClose={onReset} ref={dialog} className="result-modal">
+     {userLost && <h2>You lost </h2>}
+     {!userLost && <h2>Your score: {score} </h2>}
      <p>The target time was  <strong>{targetTime} seconds. </strong> </p>
-     <p>You stopped the timer with <strong> X seconds left. </strong>  </p>
-     <form method="dialog">
+     <p>You stopped the timer with <strong> {formattedTime} seconds left. </strong>  </p>
+     <form method="dialog" onSubmit={onReset}>
       <button>Close</button>
      </form>
     </dialog>
